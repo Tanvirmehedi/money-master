4357bdf8d6d86cf1ef58fc954d65b527d209f778
Tanvirmehedi <tjmehedi201@gmail.com>
2022-02-17 03:46:40 +0600

    Expense Calculation Done

--- js/app.js
+++ js/app.js
@@ -2,22 +2,57 @@
 document
   .getElementById("calculate-button")
   .addEventListener("click", function () {
-    getMonthlyIncome();
+    calculateBalance();
   });
 
-// GET MONTHLY EXPENSES
+document.getElementById("saving-button").addEventListener("click", function () {
+  console.log(getInputValue("saving-input-box"));
+});
 
-//   GET MONTHLY INCOME
-
-function getMonthlyIncome() {
-  const incomeInputField = document.getElementById("income-input-box");
-  const incomeInputValue = incomeInputField.value;
-  if (incomeInputValue >= 0 && incomeInputValue != 0) {
-    const incomeTextConvert = parseFloat(incomeInputValue);
-    incomeInputField.value = "";
-    return console.log(incomeTextConvert);
+//   HANDLE INPUT FIELD
+function getInputValue(id) {
+  const inputField = document.getElementById(id);
+  const inputFieldValue = inputField.value;
+  if (inputFieldValue >= 0 && inputFieldValue != "") {
+    const inputTextConvert = parseFloat(inputFieldValue);
+    inputField.value = "";
+    return inputTextConvert;
   } else {
-    errorMessage("Please Give Your Monthly Income As Number!!");
+    errorMessage("SomeThing Wrong ");
+    inputField.value = "";
+  }
+}
+
+// MONTHLY EXPENSE CALCULATION
+
+function getMonthlyExpenseTotal() {
+  const pocketMoney = getInputValue("pocketMoney-input-box");
+  const homeRent = getInputValue("homeRent-input-box");
+  const foodStuff = getInputValue("food-input-box");
+  const totalExpanse = pocketMoney + homeRent + foodStuff;
+  getDisplayOutput("display-total-expense", totalExpanse);
+  return totalExpanse;
+}
+
+// CALCULATE BALANCE AFTER EXPANSE
+function calculateBalance() {
+  const monthlyIncome = getInputValue("income-input-box");
+  const totalExpense = getMonthlyExpenseTotal();
+  if (monthlyIncome >= totalExpense) {
+    const balance = monthlyIncome - totalExpense;
+    getDisplayOutput("display-balance", balance);
+  } else {
+    errorMessage("Your Expense Is To High Control Your Self");
+  }
+}
+
+// DISPLAY RETURNS
+function getDisplayOutput(id, returnValue) {
+  const displayId = document.getElementById(id);
+  if (isNaN(returnValue) != true) {
+    displayId.innerText = returnValue;
+  } else {
+    errorMessage("Value Empty");
   }
 }
 
