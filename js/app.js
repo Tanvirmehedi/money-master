4b91338f472463055246aa1b22865ce0014664ee
Tanvirmehedi <tjmehedi201@gmail.com>
2022-02-17 02:17:55 +0600

    Monthly Income Functionality Done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log("Welcome Hisab Nikas");
+// Calculation Button Click
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    getMonthlyIncome();
+  });
+
+// GET MONTHLY EXPENSES
+
+//   GET MONTHLY INCOME
+
+function getMonthlyIncome() {
+  const incomeInputField = document.getElementById("income-input-box");
+  const incomeInputValue = incomeInputField.value;
+  if (incomeInputValue >= 0 && incomeInputValue != 0) {
+    const incomeTextConvert = parseFloat(incomeInputValue);
+    incomeInputField.value = "";
+    return console.log(incomeTextConvert);
+  } else {
+    errorMessage("Please Give Your Monthly Income As Number!!");
+  }
+}
+
+// ERROR HANDLING
+
+function errorMessage(text) {
+  alert(text);
+}
